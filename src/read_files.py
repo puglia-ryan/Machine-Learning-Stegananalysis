51dd37d12adecd0a78c07e1a01716b1eec56da80
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-02 12:41:38 +0200

    Adjusted input image size to fit the image size of the dataset and changed the logic for determining the label of the image

--- src/read_files.py
+++ src/read_files.py
@@ -2,7 +2,7 @@ from PIL import Image
 import os
 import numpy as np
 
-def read_and_resize_images(image_folder, target_size=(1024, 1024)):
+def read_and_resize_images(image_folder, target_size=(512, 512)):
     images = []
     labels = []
     for subdir, _, files in os.walk(image_folder):
@@ -13,7 +13,7 @@ def read_and_resize_images(image_folder, target_size=(1024, 1024)):
                     img_resized = img.resize(target_size, Image.ANTIALIAS)
                     images.append(np.array(img_resized))
                     # This line may differ depending of the dataset's structure/labelling
-                    labels.append(1 if 'yes' in subdir else 0)
+                    labels.append(1 if 'stego' in subdir else '0')
         images = np.array(images) / 255.0 #Normalisation
         labels = np.append(labels)
     return images, labels
