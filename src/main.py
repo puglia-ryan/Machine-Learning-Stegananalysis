b2aafd7fa0bcd7604f134d8ae04f8e3d73ca738e
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-02 13:03:53 +0200

    Changed the way that the images are fetched in main.py

--- src/main.py
+++ src/main.py
@@ -3,12 +3,15 @@ import read_files
 image_folder = 'data/images\/'
 images, labels = read_files.read_and_resize_images(image_folder)
 
-training_folder = 'data/train/train'
+training_folder_clean = 'data/train/train/clean\/'
+training_folder_stego = 'data/train/train/stego\/'
 testing_folder = 'data/test/test'
 val_folder = 'data/val/val'
 
-training_imgs, training_labels = read_files.read_and_resize_images(training_folder)
-testing_imgs, testing_labels = read_files.read_and_resize_images(testing_folder)
-val_imgs, val_labels = read_files.read_and_resize_images(val_folder)
 
+#training_imgs, training_labels = read_files.read_and_resize_images(training_folder)
+#testing_imgs, testing_labels = read_files.read_and_resize_images(testing_folder)
+#val_imgs, val_labels = read_files.read_and_resize_images(val_folder)
 
+training_imgs_clean = read_files.read_2(training_folder_clean)
+training_imgs_stego = read_files.read_2(training_folder_stego)
