1e8435be0b95ae5c6829a3694e003717a4f3eca3
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-02 12:47:56 +0200

    Changed the logic in main.py for aquiring the images and labels using the read_files function from read_files.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,12 @@ import read_files
 image_folder = 'data/images\/'
 images, labels = read_files.read_and_resize_images(image_folder)
 
-# Split the images into training and validation data sets 
-from sklearn.model_selection import train_test_split
-x_train, x_val, y_train, y_val = train_test_split(images, labels, test_size=0.2, random_state=42)
+training_folder = 'data/train/train'
+testing_folder = 'data/test/test'
+val_folder = 'data/val/val'
+
+training_imgs, training_labels = read_files.read_and_resize_images(training_folder)
+testing_imgs, testing_labels = read_files.read_and_resize_images(testing_folder)
+val_imgs, val_labels = read_files.read_and_resize_images(val_folder)
+
+
