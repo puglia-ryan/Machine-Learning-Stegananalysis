d8980ae98a70d68352bd3467f5e3d26f6a0519b2
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-08 23:13:03 +0200

    Fixed an issue in read_and_resize images. The function now makes sure that any images added are in the RGBA format. This was done because some images in the dataset are RGB and others are RGBA

--- src/read_files.py
+++ src/read_files.py
@@ -9,15 +9,17 @@ def read_and_resize_images(image_folder, target_size=(512, 512)):
     labels = []
     for subdir, _, files in os.walk(image_folder):
         for file in files:
-            if file.endswith(('.png', '.jpeg', '.jpg')):
+            if file.endswith(('.png')):
                 img_path = os.path.join(subdir, file)
                 with Image.open(img_path) as img:
+                    if img.mode == 'RGB':
+                        img = img.convert('RGBA')
+                    print(img.mode)
                     img_resized = img.resize(target_size, Image.ANTIALIAS)
-                    images.append(np.array(img_resized))
+                    images.append(np.asarray(img))
                     # This line may differ depending of the dataset's structure/labelling
                     labels.append(1 if 'stego' in subdir else '0')
-        images = np.array(images) / 255.0 #Normalisation
-        labels = np.append(labels)
+        #images = np.array(images) / 255.0 #Normalisation
     return images, labels
 
 
@@ -30,5 +32,7 @@ def read_2(folder_dir, target_size=(512, 512)):
         if (file.endswith(".png")):
             img_path = os.path.join(folder_dir, file)
             with Image.open(img_path) as img:
-                images.append(np.array(img))
+                img_resized = img.resize(target_size, Image.ANTIALIAS)
+                images.append(np.array(img_resized))
+    images = np.array(images) / 255.0 #Normalization
     return images
