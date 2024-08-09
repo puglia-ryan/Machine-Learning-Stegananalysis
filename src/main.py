ad8ee80e39d41e7648efd7650c33d2b4150ebf45
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-09 02:24:41 +0200

    Modified the main function so that the model may begin training and testing with the images from the downloaded dataset

--- src/main.py
+++ src/main.py
@@ -14,18 +14,12 @@ testing_folder = 'data/test/test'
 val_folder = 'data/val/val'
 
 
-#training_imgs, training_labels = read_files.read_and_resize_images(training_folder)
-#testing_imgs, testing_labels = read_files.read_and_resize_images(testing_folder)
+training_imgs, training_labels = read_files.read_and_resize_images(training_folder)
+testing_imgs, testing_labels = read_files.read_and_resize_images(testing_folder)
 #val_imgs, val_labels = read_files.read_and_resize_images(val_folder)
 
-#train1, train_label = read_files.read_and_resize_images("data\test_function\train")
-#test1, test_label = read_files.read_and_resize_images("data\test_function\test")
+
 model1 = model.build_model()
-model1.summary()
-train1 = np.random.rand(10, 512, 512, 4)
-test1 = np.random.rand(5, 512, 512, 4)
-train_label = np.random.randint(0, 2, 10)
-test_label = np.random.randint(0, 2, 5)
-model1.fit(train1, train_label, epochs=10, batch_size=32, validation_data=(test1, test_label))
+model1.fit(training_imgs, training_labels, epochs=10, batch_size=32, validation_data=(testing_imgs, testing_labels))
 model1.save('my_model.h5')
  
