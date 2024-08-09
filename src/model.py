2ef08e573a59dc20cc7557ad422fa507670f56cc
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-09 04:01:05 +0200

    Changed the structur eof the model

--- src/model.py
+++ src/model.py
@@ -6,18 +6,21 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def build_model():
+    num_classes = 2
+    
+    model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
 
-    model = Sequential([
-        Conv2D(32, (3, 3), activation='relu', input_shape=(512, 512, 3)),
-        MaxPooling2D((2, 2)),
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Conv2D(128, (3, 3), activation='relu'),
-        MaxPooling2D((2, 2)),
-        Flatten(),
-        Dense(128, activation='relu'),
-        Dense(1, activation='sigmoid')
-        ])
 
     model.compile(
             optimizer=Adam(),
