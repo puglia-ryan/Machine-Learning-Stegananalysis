d01a40de79476b2db173f6d0be5694961bf87fc5
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-14 23:40:41 +0200

    Refactored the code in main.py and added some more comments for a better overview for the reader

--- src/main.py
+++ src/main.py
@@ -17,10 +17,11 @@ data = tf.keras.utils.image_dataset_from_directory(
     image_size=(512, 512),
     batch_size=16)
 
-data = data.map(lambda x, y: (x/255, y)) #Normalises the data from range 0-255 to 0-1
+data = data.map(lambda x, y: (x/255, y)) #Normalises the data from range 0-255 to range 0-1
 data_iterator = data.as_numpy_iterator()
 batch = data_iterator.next()
 
+#The data is split into training, testing and validation sets
 train = data.take(int(len(data)*0.7))
 test = data.take(int(len(data)*0.2)+1)
 val = data.take(int(len(data)*0.1)+1)
@@ -31,20 +32,27 @@ data = data.cache().prefetch(buffer_size=AUTOTUNE)
 #The number in the brackets denotes which model structure was used from the model.py file
 checkpoint_path = "my_model2(2).h5"
 
-
+#If the model with the given names exists, it is loaded in
 if os.path.exists(checkpoint_path):
     model1 = load_model(checkpoint_path)
 else:
+    #else the model will be built
     model1 = model.build_model_2()
 
 logdir = 'logs'
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+
+#The fit method allows the model to train and improve
 model1.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+
+#Following code block is to determine the accuracy of the model
 true_labels = np.concatenate([y for x, y in test], axis=0)
 predictions = model1.predict(test)
 predicted_classes = np.argmax(predictions, axis=1)
 accuracy = accuracy_score(true_labels, predicted_classes)
 print(f'Accuracy: {accuracy * 100:2f}%')
+
+#After training or prediciting, the model is once again saved
 model1.save(checkpoint_path) 
 
 """
