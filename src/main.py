0ba48eb1db8882fc4667e64eb17abc2dc439f498
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-14 22:56:57 +0200

    Added the ability for the model to predict on the test data set

--- src/main.py
+++ src/main.py
@@ -13,29 +13,35 @@ import matplotlib.pyplot as plt
 
 data = tf.keras.utils.image_dataset_from_directory(
     'data',
-    validation_split= 0.2,
-    subset="training",
-    seed=123,
-    image_size=(512, 512))
-
-val_ds = tf.keras.utils.image_dataset_from_directory(
-  'data',
-  validation_split=0.2,
-  subset="validation",
-  seed=123,
-  image_size=(512, 512))
+    image_size=(512, 512),
+    batch_size=16)
 
 data = data.map(lambda x, y: (x/255, y)) #Normalises the data from range 0-255 to 0-1
 data_iterator = data.as_numpy_iterator()
 batch = data_iterator.next()
 
+train = data.take(int(len(data)*0.7))
+test = data.take(int(len(data)*0.2)+1)
+val = data.take(int(len(data)*0.1)+1)
+
+
 AUTOTUNE = tf.data.AUTOTUNE
 data = data.cache().prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-model1 = model.build_model()
-model1.fit(data, validation_data=val_ds, epochs=3)
-model1.save('my_model.h5')
+checkpoint_path = "my_model.h5"
+
+
+if os.path.exists(checkpoint_path):
+    model1 = load_model(checkpoint_path)
+else:
+    model1 = model.build_model()
+
+logdir = 'logs'
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+#model1.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+predictions = model1.predict(test)
+model1.save('my_model.h5') 
+
 """
 training_imgs, training_labels = read_files.read_and_resize_images(training_folder)
 print("Done loading training images and labels")
