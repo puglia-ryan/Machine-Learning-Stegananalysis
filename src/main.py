da8c75765a598c7b921e952ef9f8b0e1d2cda561
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-14 23:17:51 +0200

    Made it possible to see the accuracy of the model

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from PIL import Image
 import numpy as np
 import model
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
@@ -39,7 +40,11 @@ else:
 logdir = 'logs'
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
 #model1.fit(train, epochs=20, validation_data=val, callbacks=[tensorboard_callback])
+true_labels = np.concatenate([y for x, y in test], axis=0)
 predictions = model1.predict(test)
+predicted_classes = np.argmax(predictions, axis=1)
+accuracy = accuracy_score(true_labels, predicted_classes)
+print(f'Accuracy: {accuracy * 100:2f}%')
 model1.save('my_model.h5') 
 
 """
