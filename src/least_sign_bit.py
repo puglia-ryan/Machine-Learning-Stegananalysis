7f3e143ab3d702b9384d3f757a0c1fed77ea98b3
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-15 14:34:47 +0200

    Aded the ability to turn the desired image into a Numpy array for better manipulation

--- src/least_sign_bit.py
+++ src/least_sign_bit.py
@@ -2,5 +2,7 @@ from PIL import Image
 import numpy as np
 
 def encode_lsb(image_path, message, output_path):
-
+        #Load the image
+        img = Image.open(image_path)
+        img_array = np.array(img)
     return
