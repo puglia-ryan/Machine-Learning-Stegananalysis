ac329d8d022d42d41cd3ec125febc04edb598c58
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-15 14:39:16 +0200

    Added a flattened version of the image array. This will allow for easier bit manipulation down the line

--- src/least_sign_bit.py
+++ src/least_sign_bit.py
@@ -2,11 +2,12 @@ from PIL import Image
 import numpy as np
 
 def encode_lsb(image_path, message, output_path):
-        #Load the image
-        img = Image.open(image_path)
-        img_array = np.array(img)
+    #Load the image
+    img = Image.open(image_path)
+    img_array = np.array(img)
+    flat_img_array = img_array.flatten()
 
-        #The message is encoded into binary
-        message_in_bin = ''.join([format(ord(char), '80b') for char in message])
-        
-    return
+    #The message is encoded into binary
+    message_in_bin = ''.join([format(ord(char), '80b') for char in message])
+    
+    return 1
