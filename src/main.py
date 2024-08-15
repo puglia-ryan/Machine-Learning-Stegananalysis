4c3bcdbb6559011722c6559788a25d8a9a3b359a
puglia-ryan <ryan.puglia.001@student.uni.lu>
2024-08-15 15:48:40 +0200

    Prepared main.py to encode the images in data/clean using the functions implemented in least_sign_bit.py

--- src/main.py
+++ src/main.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import least_sign_bit as lsb
 
 lsb.encode_lsb("data/clean/00001.png", "Hello There", "data/personal_stego/steg_00001.png")
-
+lsb.encode_all_img_in_folder("data/clean", "data/personal_stego")
 """
 data = tf.keras.utils.image_dataset_from_directory(
     'data',
